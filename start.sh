6abefd148fb00c0bb9780787398e2d3870e37b1a
Andrey Makarov <andrey.makarov@flant.ru>
2013-11-23 03:31:06 +0400

    clear code & new

--- start.sh
+++ start.sh
@@ -12,7 +12,9 @@ GIT_BRANCH='master'
 USER_NAME='deploy'
 USER_PASSWORD='12345'
 USER_SHELL='/bin/bash'
-USER_HOME='/home/$user'
+USER_HOME='/home/deploy/$user'
+
+RUBY_VERSION='2.0.0-p247'
 
 NGINX_LISTEN_ADDRESS='0.0.0.0'
 NGINX_LISTEN_PORT='80'
@@ -24,7 +26,7 @@ usage() {
 
 	printf "Использование:\n\n"
 	printf "\t-r | --run\tСделай мне хорошо!\n"
-	printf "\t-g | --generate\tСгенерировать конфиг\n"
+	printf "\t-v | --view\tПросмотр хостов\n"
 	printf "\t-h | --help\tЭто сообщение\n"
 }
 
@@ -34,10 +36,11 @@ edit_vars() {
 	read -e -p "Git url: " -i "$GIT_URL" GIT_URL
 	read -e -p "Branch name for git checkout: " -i "$GIT_BRANCH" GIT_BRANCH
 
-	read -e -p "User name: " -i "$USER_NAME" USER_NAME
+	read -e -p "User name: " -i "$USER_NAME" USER
 	read -e -p "User password: " -i "$USER_PASSWORD" USER_PASSWORD
 	read -e -p "User shell: " -i "$USER_SHELL" USER_SHELL
-	read -e -p "User home: " -i "$USER_HOME" USER_HOME
+
+	read -e -p "Ruby version: " -i "$RUBY_VERSION" RUBY_VERSION
 
 	read -e -p "Nginx listen address: " -i "$NGINX_LISTEN_ADDRESS" NGINX_LISTEN_ADDRESS
 	read -e -p "Nginx listen port: " -i "$NGINX_LISTEN_PORT" NGINX_LISTEN_PORT
@@ -61,6 +64,8 @@ generate_config() {
 	printf "user_password: \"%s\"\n" "$USER_PASSWORD" >> $SETTINGS
 	printf "user_shell: \"%s\"\n" "$USER_SHELL" >> $SETTINGS
 	printf "home: \"%s\"\n" "$USER_HOME" >> $SETTINGS
+
+	printf "ruby_version: \"%s\"\n" "$RUBY_VERSION" >> $SETTINGS
 
 	printf "\n### Config files\n" >> $SETTINGS
 
@@ -97,6 +102,66 @@ view_hosts_files() {
 
 }
 
+ssh_ls_apps(){
+		
+		for h in `cat hosts/${HOST_FILES[$1]}`; do
+			host_name=`echo $h | awk -F: '{print $1}'`
+			port=`echo $h | awk -F: '{print $2}'`
+			printf "Host: %s\n" "$host_name"
+			ssh $host_name -p `[ $port ] && echo $port || echo 22` 'ls -d /home/*/*/www' 2>/dev/null
+			echo
+		done
+
+}
+
+list_all_installed_apps() {
+
+	clear
+
+	for f in ${HOST_FILES[@]}; do
+		printf "Host file: %s\n\n" "$f"
+		ssh_ls_apps $f
+	done
+}
+
+list_apps() {
+
+	printf "Host-файлы: %s\n" "${#HOST_FILES[@]}"
+
+	j=0
+
+	for i in ${HOST_FILES[@]}
+		do			
+			printf "\n$j) $i\n"
+			let j++
+		done
+
+	printf "\nВведите номер интересующего вас host-файла...\nИли просмотреть их все? (a)\n"
+	read number
+
+	if [ $number = 'a' ]
+		then
+			list_all_installed_apps
+		else
+			clear
+			ssh_ls_apps $number
+			
+			echo "Удаляем эти приложения? y/n"
+			read yn
+
+			[ $yn = 'y' ] && ( delete_apps hosts/"${HOST_FILES[$number]}" )
+	fi
+
+}
+
+delete_apps() {
+
+	echo "Введите пароль от sudo ..."
+	ansible-playbook -i $1 remove.yml -K
+
+}
+
+
 run_playbook() {
 
 	echo "Введите пароль от sudo ..."
@@ -110,13 +175,17 @@ case $1 in
 		usage
 		;;
 
-	-r|--run)
+	-v|--view)
 		view_hosts_files
 		;;
 
 	-g|--generate)
 		edit_vars
 		generate_config
+		;;
+
+	-a|--apps)
+		list_apps
 		;;
 
 	*)
