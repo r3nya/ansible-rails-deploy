8651071639a1fc23c98c641373f86646de27e38e
Andrey Makarov <andrey.makarov@flant.ru>
2013-11-22 15:20:07 +0400

    small fixz

--- start.sh
+++ start.sh
@@ -34,11 +34,7 @@ edit_vars() {
 	read -e -p "Git url: " -i "$GIT_URL" GIT_URL
 	read -e -p "Branch name for git checkout: " -i "$GIT_BRANCH" GIT_BRANCH
 
-<<<<<<< HEAD
-	read -e -p "User name: " -i "$USER_NAME" USER
-=======
 	read -e -p "User name: " -i "$USER_NAME" USER_NAME
->>>>>>> b586bd16ea24a623000a32a476c073d636351fce
 	read -e -p "User password: " -i "$USER_PASSWORD" USER_PASSWORD
 	read -e -p "User shell: " -i "$USER_SHELL" USER_SHELL
 	read -e -p "User home: " -i "$USER_HOME" USER_HOME
